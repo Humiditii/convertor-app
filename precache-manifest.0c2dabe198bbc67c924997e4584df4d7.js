02e75341898fbf47da878943a946eb8fe9d7c3e7
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-06-19 22:26:43 +0000

    Updates

---
+++ precache-manifest.0c2dabe198bbc67c924997e4584df4d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be36b6c6aa9637006696021831a1bad7",
+    "url": "/convertor-app/index.html"
+  },
+  {
+    "revision": "528e53faad02ea2f9960",
+    "url": "/convertor-app/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "ec4b1fb197b616e34fbb",
+    "url": "/convertor-app/static/css/main.4b6c7a8b.chunk.css"
+  },
+  {
+    "revision": "528e53faad02ea2f9960",
+    "url": "/convertor-app/static/js/2.ed7d7e67.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/convertor-app/static/js/2.ed7d7e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4b1fb197b616e34fbb",
+    "url": "/convertor-app/static/js/main.cae71fd6.chunk.js"
+  },
+  {
+    "revision": "e0952d630d066c0d6a03",
+    "url": "/convertor-app/static/js/runtime-main.30d4b6e5.js"
+  }
+]);
